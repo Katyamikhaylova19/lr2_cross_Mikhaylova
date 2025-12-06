2671ac23b7c251f8b4568e968a29c9814f7bf164
Katyamikhaylova19 <megalogin32015@gmail.com>
2025-12-06 21:34:36 +0300

    Add Postman Collection

--- Mikhaylova_lr2/obj/Debug/net8.0/Mikhaylova_lr2.AssemblyInfo.cs
+++ Mikhaylova_lr2/obj/Debug/net8.0/Mikhaylova_lr2.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Mikhaylova_lr2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d1e2b2a4fdd432174fb84d58b87a4016e5570f7c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1c92d5ec29dea7a641cfdac3b9222101b62fa58e")]
 [assembly: System.Reflection.AssemblyProductAttribute("Mikhaylova_lr2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Mikhaylova_lr2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
